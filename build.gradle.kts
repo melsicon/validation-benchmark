b387fe3b6f43a23ee5ccc8e8a0da77963cbc40d3
Oliver Eikemeier <eikemeier@fillmore-labs.com>
2021-07-03 01:07:41 +0200

    Update dapendencies
    Signed-off-by: Oliver Eikemeier <eikemeier@fillmore-labs.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,20 +35,20 @@ dependencies {
   compileOnly("org.projectlombok:lombok:1.18.20")
   annotationProcessor("org.projectlombok:lombok:1.18.20")
 
-  compileOnly("org.immutables:value-annotations:2.9.0-beta2")
-  annotationProcessor("org.immutables:value-processor:2.9.0-beta2")
+  compileOnly("org.immutables:value-annotations:2.9.0-beta3")
+  annotationProcessor("org.immutables:value-processor:2.9.0-beta3")
 
   implementation("org.openjdk.jmh:jmh-core:1.32")
   annotationProcessor("org.openjdk.jmh:jmh-generator-annprocess:1.32")
 
-  compileOnly("org.checkerframework:checker-qual:3.13.0")
-  testCompileOnly("org.checkerframework:checker-qual:3.13.0")
+  compileOnly("org.checkerframework:checker-qual:3.15.0")
+  testCompileOnly("org.checkerframework:checker-qual:3.15.0")
 
   implementation("com.google.guava:guava:30.1.1-jre")
   implementation("jakarta.validation:jakarta.validation-api:2.0.2")
   runtimeOnly("org.glassfish:jakarta.el:3.0.3")
   runtimeOnly("org.hibernate.validator:hibernate-validator:6.2.0.Final")
-  runtimeOnly("org.ow2.asm:asm:9.1")
+  runtimeOnly("org.ow2.asm:asm:9.2")
 
   testImplementation("junit:junit:4.13.2")
   testImplementation("com.google.truth:truth:1.1.3")
